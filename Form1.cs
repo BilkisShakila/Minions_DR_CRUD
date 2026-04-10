2323b1ab7ccde32bceafee47181516d68aa085e0
BilkisShakila <bilkisshakilaa@gmail.com>
2026-04-10 10:20:14 +0700

    Membuat constructur

--- Form1.cs
+++ Form1.cs
@@ -1,14 +1,17 @@
 ﻿using System;
-using System.Data.SqlClient
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace CRUDMahasiswaADO
 {
     public partial class Form1 : Form
     {
+        private readonly SqlConnection conn;
+        private readonly string connectionString = "Data Source=LAPTOP-LQHDCSVJ\\BILKISAQILA;Initial Catalog=DBAkademikADO;Integrated Security=True";
         public Form1()
         {
             InitializeComponent();
+            conn = new SqlConnection(connectionString);
         }
 
         private void label4_Click(object sender, EventArgs e)
